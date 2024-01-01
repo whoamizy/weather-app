ea4d23ca6c024ff140e70c115ba2855096bda3ab
whoamizy <delovoysergei098@gmail.com>
2024-01-01 15:16:46 +0400

    feat: add home header, add styles and variables

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
+  <HomeHeader />
   <div>Home</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HomeHeader from '@/components/HomeHeader.vue'
+</script>
 
 <style lang="scss" scoped></style>
