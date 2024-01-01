28cc8af0c0a2cc491ff2637f744d1c8018303563
whoamizy <delovoysergei098@gmail.com>
2024-01-01 15:25:36 +0400

    feat: add home welcome

---
+++ src/components/HomeWelcome.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="welcome">
+    <h1 class="welcome__title">Welcome to <span class="welcome__accent">TypeWeather</span></h1>
+    <p class="welcome__description">Choose a location to see the weather forecast</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.welcome {
+  text-align: center;
+  margin-bottom: 56px;
+  &__title {
+    font-weight: 700;
+    font-size: 32px;
+    margin-bottom: 8px;
+  }
+  &__description {
+    font-size: 20px;
+    color: var(--text-secondary-light);
+  }
+  &__accent {
+    color: var(--text-accent);
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <template>
   <HomeHeader />
+  <HomeWelcome />
   <div>Home</div>
 </template>
 
 <script setup lang="ts">
 import HomeHeader from '@/components/HomeHeader.vue'
+import HomeWelcome from '@/components/HomeWelcome.vue'
 </script>
 
 <style lang="scss" scoped></style>
