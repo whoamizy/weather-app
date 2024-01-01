70130e7fa91e222c3b332f7c2890467cd37e6869
whoamizy <delovoysergei098@gmail.com>
2024-01-01 16:22:32 +0400

    feat: add base input

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="wrapper">
+    <input
+      :class="['input', { 'input-loading': isLoading }]"
+      type="text"
+      :value="value"
+      @input="updateValue"
+      :placeholder="placeholder"
+    />
+    <div class="loader" v-if="isLoading">
+      <BaseLoader />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseLoader from './BaseLoader.vue'
+
+defineProps({
+  value: {
+    type: String,
+    required: true
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  },
+  isLoading: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emit = defineEmits(['update:value'])
+
+const updateValue = (e: Event) => {
+  const target = e.target as HTMLInputElement
+
+  emit('update:value', target.value)
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  position: relative;
+  width: 100%;
+}
+.input {
+  width: 100%;
+  padding: 17px 20px;
+  border-radius: 8px;
+  background-color: var(--input-bg-color);
+  font-size: 16px;
+  &-loading {
+    padding-right: 72px;
+  }
+  &::placeholder {
+    color: var(--input-placeholder);
+  }
+}
+.loader {
+  width: 32px;
+  height: 32px;
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 10;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,24 @@
 <template>
   <HomeHeader />
-  <HomeWelcome />
-  <div>Home</div>
+  <div class="content">
+    <HomeWelcome />
+    <BaseInput v-model:value.trim="searchVal" placeholder="Search location" :is-loading="true" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import BaseInput from '@/components/BaseInput.vue'
 import HomeHeader from '@/components/HomeHeader.vue'
 import HomeWelcome from '@/components/HomeWelcome.vue'
+import { ref } from 'vue'
+
+const searchVal = ref('')
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.content {
+  max-width: 544px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+</style>
