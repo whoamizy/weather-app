39693aa02fa657ac18262c505df915d7c59c4a8c
whoamizy <delovoysergei098@gmail.com>
2024-01-02 00:14:32 +0400

    feat: add navigate to city view

---
+++ src/views/CityView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>City {{ cityName }}</div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const cityName = route.params.name
+</script>
+
+<style lang="scss" scoped></style>
