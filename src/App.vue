4c92ff6aa8078b6fe71bcde81cdbb26cd1dd41b1
ㅤㅤㅤㅤㅤ <85992977+whoamizy@users.noreply.github.com>
2024-01-02 20:30:23 +0400

    feat: add transitions (#6)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <main>
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Transition>
+        <Component :is="Component" />
+      </Transition>
+    </RouterView>
   </main>
 </template>
